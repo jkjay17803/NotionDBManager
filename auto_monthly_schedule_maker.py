920f7cb04315bb2526972e71b4d6a65bd98528b4
jkjay17803 <jkjay17803@gmail.com>
2025-09-04 19:08:02 +0900

    auto monthly-schedule_Maker!

--- auto_monthly_schedule_maker.py
+++ auto_monthly_schedule_maker.py
@@ -1,14 +1,32 @@
+import datetime
+
 from dotenv import load_dotenv
 import os
 import json
 import requests
-from datetime import date, timedelta
+from datetime import date, timedelta,timezone
 import calendar
+from dataclasses import dataclass
+
 load_dotenv()
 
 NOTION_TOKEN = os.getenv("NOTION_API_KEY")
 DATABASE_ID = "25c463560c4d80bf989be04203a6891e"
+
+title = str(input("Enter the title: "))
+d = input("Enter the days\nMon:0 Tue:1 Wed:2 Thu:3 Fri:4 Sat:5 Sun:6\n:")
+lst = list(map(int, d.split()))
+
+data = input("Enter the startTime: ")
+start = [int(x) for x in data.split(":")]
+data = input("Enter the endTime: ")
+end = [int(x) for x in data.split(":")]
+
+start[0] -= 9
+end[0] -= 9
+
 today = date.today()
+m = today.month
 year, month = today.year, today.month
 
 # 이번 달 마지막 날
@@ -18,9 +36,46 @@ last_day = calendar.monthrange(year, month)[1]
 weekend_days = [
     day
     for day in range(1, last_day + 1)
-    if date(year, month, day).weekday() in (4, 5, 6)  # 금=4, 토=5, 일=6
+    if date(year, month, day).weekday() in (lst)  # 금=4, 토=5, 일=6
 ]
 
+priority = int(input("Enter the priority\nHigh: 0 Medium: 1 Low: 2\n:"))
+if priority == 0:
+    priority = "높음"
+elif priority == 1:
+    priority = "중간"
+elif priority == 2:
+    priority = "낮음"
+
+
+category = int(input("Enter the category\nPersonal: 0 || Cafe: 1 || Study:2 || University: 3 || Love: 4 || Work: 5\n:"))
+if category == 0:
+    category = "개인"
+elif category == 1:
+    category = "카페"
+elif category == 2:
+    category = "공부"
+elif category == 3:
+    category = "대학"
+elif category == 4:
+    category = "연애"
+elif category == 5:
+    category = "직장"
+
+
+#확인
+print("\n\n=====[ 현재 설정 ]======")
+print(f"제목: {title}")
+print(f"요일코드: {d}")
+print(f"시간: {start[0]+9}:{start[1]} ~ {end[0]+9}:{end[1]}")
+print(f"우선도 코드: {priority}")
+print(f"카테고리 코드: {category}")
+print("\n입니다. 새로운 항목으로 자동 생성할까요? (y/n)")
+
+yn = input(":")
+if yn == "n" or yn == "ㅜ":
+    print("지금까지의 내용을 모두 잊고 종료합니다.")
+    exit(0)
 
 url = "https://api.notion.com/v1/pages"  # 페이지 생성 URL로 변경
 
@@ -42,24 +97,24 @@ for days in weekend_days:
             "내용": {
                 "title": [
                     {
-                        "text": { "content": "카페 알바" }
+                        "text": { "content": f"{title}" }
                     }
                 ]
             },
             "날짜": {
                 "date": {
-                    "start": f"2025-09-{days:02d}T11:00:00",
-                    "end": f"2025-09-{days:02d}T14:59:00"
+                    "start": f"2025-{m}-{days:02d}T{int(start[0]):02d}:{int(start[1]):02d}:00",
+                    "end": f"2025-{m}-{days:02d}T{int(end[0]):02d}:{int(end[1]):02d}:00"
                 }
             },
             "우선순위": {
                 "select": {
-                    "name":"높음"
+                    "name":f"{priority}"
                 }
             },
             "카테고리": {
                 "select": {
-                    "name":"카페"
+                    "name":f"{category}"
                 }
             }
         }
@@ -70,8 +125,7 @@ for days in weekend_days:
 
     # 6. 결과 확인
     if response.status_code == 200:
-        print("데이터가 성공적으로 추가되었습니다!")
-        print(response.json())
+        print("[+] 데이터가 성공적으로 추가되었습니다!")
     else:
-        print("데이터 추가 실패:", response.status_code)
+        print("[!] 데이터 추가 실패:", response.status_code)
         print(response.text)
